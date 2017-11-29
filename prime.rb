9f61f34fb7bca17632533e68f7a254463cb44e86
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 19:41:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ def prime?(n)
   range.map {|i|
     n % i == 0
   }
-  range.size == 2 ? true : false
+  range.size == 2 ? && range[-1] == n true : false
 
 end
 puts Benchmark.measure { BigMath.PI(10_000)}
