d0284791975cb2dfa4760e4404aa26af2f339392
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 19:38:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,11 @@ require "benchmark"
 require "bigdecimal/math"
 
 def prime?(n)
-  range = (1..n).to_a
-  range.map {|i|
-    n % i == 0 ? i : nil
+  divisble_by = []
+  (1..n).to_a.each {|i|
+    n % i == 0 ? divisble_by << i : nil
   }
-  range.length == 2 && range.any? {|d| d == nil
-  } ? true : false
-  
+divisble_by.size == 2 ? true : false
+
 end
 puts Benchmark.measure { BigMath.PI(10_000)}
