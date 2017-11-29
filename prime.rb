8ad3ebb868aa31ee01301c1ad2bb3a2ef32b12de
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 19:19:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,9 @@
 def prime?(n)
   divisble_by = []
   (1..n).to_a.each {|i|
-    n % i == 0 ? divisble_by << i : nil
+    if n % i == 0
+      divisble_by << i
+    end
   }
 divisble_by.size == 2 ? true : false
 
