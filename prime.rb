3d8f095291570d8648fa567540a76b5ddb40adf7
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 19:11:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
 # Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
+
+# calculate pi to 10k digits
+puts Benchmark.measure { BigMath.PI(10_000) }
+
 def prime?(n)
   divisble_by = []
   (1..n).to_a.each {|i|
@@ -9,9 +15,3 @@ def prime?(n)
 divisble_by.size == 2 ? true : false
 
 end
-
-require 'benchmark'
-require 'bigdecimal/math'
-
-# calculate pi to 10k digits
-puts Benchmark.measure { BigMath.PI(10_000) }
