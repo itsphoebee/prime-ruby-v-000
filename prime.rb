d4d91d10f52b7bfb7f2c223e12b3e762b1d8e4ff
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 19:31:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,10 @@ require "bigdecimal/math"
 
 def prime?(n)
   range = (1..n).to_a
-  range.map {|i|
-    n % i == 0
+  range.each {|i|
+    n % i == 0 ? ran << i : nil
   }
 range.size == 2 ? true : false
 
 end
-
 puts Benchmark.measure { BigMath.PI(10_000)}
