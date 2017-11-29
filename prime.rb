f8399feb0f70b2e4ac4cb0d905e8d1bb362f5025
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 20:36:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,9 @@
 require "benchmark"
 require "bigdecimal/math"
 
-def prime? (n)
-  for d in 2..(n - 1)
-   if (n % d) == 0
-    return false
-   end
-  end
-
-  true
- end
+def prime?(n)
+  (2..n/2).none?{|i| n % i == 0}
+end
 
 
 #Researched method
