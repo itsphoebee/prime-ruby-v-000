02ee0d19cb644419c7dda43c564e3a66c73d8a54
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 19:28:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,12 +5,9 @@ require "bigdecimal/math"
 def prime?(n)
   divisble_by = []
   (1..n).to_a.each {|i|
-    if n % i == 0
-      divisble_by << i
-    else nil
-    end
+    n % i == 0 ? divisble_by << i : nil
   }
-  divisble_by.size == 2 ? true : false
-end
+divisble_by.size == 2 ? true : false
 
+end
 puts Benchmark.measure { BigMath.PI(10_000)}
