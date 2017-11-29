11636758528094970d8ce5fbda8e6cba40c34d52
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 20:40:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,16 @@ require "benchmark"
 require "bigdecimal/math"
 
 def prime?(n)
-   range = (2..n/2).to_a
-   n > 1 && range.none? { |i| n % i ==0}
- end
+  divisble_by =(2..n/2).to_a
+  divisble.none?{|i| x % i == 0}
+end
+
+
+#Researched method
+#def prime?(n)
+#   range = (2..n/2).to_a
+#   n > 1 && range.none? { |i| n % i ==0}
+# end
 
 
 #initial code
