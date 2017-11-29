830b7dd1d84547a220a67f4e6d23f4e30eedb03d
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 20:42:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,6 @@
 require "benchmark"
 require "bigdecimal/math"
 
-def prime?(n)
-  divisble_by =(2..n/2).to_a
-  n > 1 && divisble_by.none?{|i| n % i == 0}
-end
 
 
 #Researched method
@@ -16,12 +12,12 @@ end
 
 
 #initial code
-#def prime?(n)
-#   divisble_by = []
-#   (1..n).to_a.each {|i|
-#     n % i == 0 ? divisble_by << i : nil
-#   }
-# divisble_by.size == 2 ? true : false
-# end
+def prime?(n)
+   divisble_by = []
+   (1..n).to_a.each {|i|
+     n % i == 0 ? divisble_by << i : nil
+   }
+ divisble_by.size == 2 ? true : false
+ end
 
 puts Benchmark.measure { BigMath.PI(10_000)}
