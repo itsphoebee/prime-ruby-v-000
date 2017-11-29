afa430acbca8ba5b46d32cbac714600cba9cff9c
Phoebe Lau <phoebelau7@gmail.com>
2017-11-29 20:44:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,20 +4,20 @@ require "bigdecimal/math"
 
 
 
-#Researched method
-#def prime?(n)
-#   range = (2..n/2).to_a
-#   n > 1 && range.none? { |i| n % i ==0}
-# end
-
-
-#initial code
+#Researched method - prime# has no positive divisors other than itself and 1.
 def prime?(n)
-   divisble_by = []
-   (1..n).to_a.each {|i|
-     n % i == 0 ? divisble_by << i : nil
-   }
- divisble_by.size == 2 ? true : false
- end
+  divisble_by =(2..n/2).to_a
+  n > 1 && divisble_by.none?{|i| n % i == 0}
+end
+
+
+#initial code - slower
+#def prime?(n)
+#   divisble_by = []
+#   (1..n).to_a.each {|i|
+#     n % i == 0 ? divisble_by << i : nil
+#   }
+# divisble_by.size == 2 ? true : false
+# end
 
 puts Benchmark.measure { BigMath.PI(10_000)}
